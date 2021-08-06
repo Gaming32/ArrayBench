ef4155a0bd14d88a2202060d2a1e11edeb8d2a6d
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-08-06 15:39:42 -0500

    Add command-line arguments

--- arraybench/src/main/java/io/github/arraybench/main/ArrayBench.java
+++ arraybench/src/main/java/io/github/arraybench/main/ArrayBench.java
@@ -1,7 +1,32 @@
 package io.github.arraybench.main;
 
+import java.io.File;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.Argument;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 public class ArrayBench {
+    static File existingFile(ArgumentParser parser, Argument arg, String value) throws ArgumentParserException {
+        File file = new File(value);
+        if (!file.exists()) {
+            throw new ArgumentParserException(file + " does not exist", parser);
+        }
+        return file;
+    }
+
     public static void main(String[] args) {
-        
+        ArgumentParser parser = ArgumentParsers.newFor("ArrayBench").build()
+            .defaultHelp(true)
+            .description("Benching program for ArrayV");
+
+        parser.addArgument("sortFile").metavar("SORT_FILE")
+            .type(ArrayBench::existingFile)
+            .help("The .java file of the sort");
+
+        Namespace ns = parser.parseArgsOrFail(args);
+        System.out.println(ns);
     }
 }
